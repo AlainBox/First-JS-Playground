ecf14bb481e9c94540632297ec189c80d7bfd917
AlainBox <93340821+AlainBox@users.noreply.github.com>
2021-12-20 17:34:18 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hello Winc Academy");
+
+let name = 'Alain';
+console.log("Alain")
+
+console.log("Not Possible, because if is a named keyword")
+
+let firstAddition = 4 + 4;
+console.log("8")
+
+let secondAddition = "4" + "4";
+console.log("44")
+
+let thirdAddition = 10 + 80;
+console.log("90")
+
+let subtraction = 20 - 14;
+console.log("6")
+
+let multiplication = 15 * 5;
+console.log("75")
+
+let division = 30 / 10;
+console.log("3")
+
+let modulus = 10 % 2;
+console.log("0")
+
+let ageFirst = 24
+console.log("number")
+
+let ageSecond = 'twenty-four';
+console.log("string")
